5d99dcd33f172daf55896cbdc22e5b54858411c5
victornunes-off <victorgabrielnunes21@gmail.com>
2025-10-25 18:28:00 -0400

    Criação da rota2

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask
 appVictor = Flask(__name__)
 
 @appVictor.route('/')
-@appVictor.route('/ola')
+@appVictor.route('/rota1')
 def raiz():
     return 'Olá, professora!'
+
+@appVictor.route('/rota2')
+def rota2():
+    resposta = "<H3> Página da rota 2 <H3>"
+    return resposta
 
 def saudacoes (nome):
     return f'Olá, {nome}!'
