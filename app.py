56f0424d01cecdc1569134574689709e069394c3
victornunes-off <victorgabrielnunes21@gmail.com>
2025-10-25 20:00:15 -0400

    Envio dos dados de app.py para usuario.html

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ appVictor = Flask(__name__, template_folder= "templates")
 def homepage():
     return render_template ("homepage.html")
 
+@appVictor.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@appVictor.route('/usuario')
+def dados_usuarios():
+    dados_usu = {"nome": "Victor", "profissao": "Programador", "empresa": "FCR"}
+    return render_template ("usuario.html", dados =dados_usu) 
+
 if __name__ == '__main__':
     appVictor.run(port = 8000, debug = True)
